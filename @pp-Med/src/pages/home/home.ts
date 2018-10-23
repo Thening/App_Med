99bf9848988a4f74018afc83421277101767b878
Thening <nctdiouf@gmail.com>
2018-10-23 12:24:45 +0000

    ajout du bouton deconnexion

--- @pp-Med/src/pages/home/home.ts
+++ @pp-Med/src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ConnexionPage } from '../connexion/connexion';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
   constructor(public navCtrl: NavController) {
 
+  }
+  deconnexion(){
+    this.navCtrl.push(ConnexionPage);
   }
 
 }
